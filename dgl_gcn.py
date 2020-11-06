25b16f2a25caf789ac707caa3ace0f9b4b2ab822
jisu <jsroh1013@gmail.com>
2020-11-06 14:50:32 +0900

    DGL tutorial-GCN

---
+++ dgl_gcn.py
@@ -0,0 +1,56 @@
+import dgl
+import dgl.function as fn
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+from dgl import DGLGraph
+
+gcn_msg = fn.copy_src(src='h',out='m') 
+gcn_reduce = fn.sum(msg='m',out='h')
+
+class GCNLayer(nn.Module):
+    def __init__(self, in_feats, out_feats):
+        super(GCNLayer,self).__init__()
+        self.linear=nn.Linear(in_feats, out_feats)
+        
+    def forward(self,g,feature):
+        with g.local_scope():
+            g.ndata['h']=feature
+            g.update_all(gcn_msg, gcn_reduce)
+            h=g.ndata['h']
+            return self.linear(h)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.layer1 = GCNLayer(1433, 16)
+        self.layer2 = GCNLayer(16, 7)
+
+    def forward(self, g, features):
+        x = F.relu(self.layer1(g, features))
+        x = self.layer2(g, x)
+        return x
+net = Net()
+print(net)
+
+from dgl.data import citation_graph as citegrh
+import networkx as nx
+def load_cora_data():
+    data = citegrh.load_cora()
+    features = th.FloatTensor(data.features)
+    labels = th.LongTensor(data.labels)
+    train_mask = th.BoolTensor(data.train_mask)
+    test_mask = th.BoolTensor(data.test_mask)
+    g = DGLGraph(data.graph)
+    return g, features, labels, train_mask, test_mask
+
+def evaluate(model, g, features, labels, mask):
+    model.eval()
+    with th.no_grad():
+        logits = model(g, features)
+        logits = logits[mask]
+        labels = labels[mask]
+        _, indices = th.max(logits, dim=1)
+        correct = th.sum(indices == labels)
+        return correct.item() * 1.0 / len(labels)
+
